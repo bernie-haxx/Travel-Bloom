8afc8d822d5c57060b88bcea4efb468cafd4696e
bernie-haxx <benkaranja43@gmail.com>
2024-11-15 10:22:13 +0300

    Optimize LookUp Function

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
 const searchButton = document.getElementById("searchBtn");
-var searchList = [];
+var searchList = {};
 
 // Search Destination and keyword
 function searchDestination() {
     // Retrieve values frome the input
     const searchText = document.getElementById("searchInput").value.toLowerCase();
     const resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "";
 
     fetch("travel_recommendation_api.json")
         // Convert into json
         .then((response) => response.json())
+
         // Parse Data
         .then((data) => {
             // Initiate Search
             // console.log(data);
-        
+            
+            // Value Checker
             if (searchText.length == 0) {
                 console.log("Please Insert Text");
                 return;
             }
-            console.log(lookUp(data, searchText));
+
+            const destination = lookUp(data, searchText);
+            console.log(destination);
     
+            // Value Checker
+            if (destination.length !== 0) {
+                displayResult(destination);
+                searchList = [];
+            } else {
+                console.log("Enter a valid Destination");
+                return;
+            }
+            
             
         })
+        // Catch Error Raised and output in the console
         .catch((e) => {
             console.log("Error:", e);
         });
 };
 
+// This is the Look UP function for finding the destination via search input 
 function lookUp(array, input) {
     for (let key in array) {
         var value = array[key];
         // Check if the input is a (country, beach and temple keyword)
         if (input === key.toLowerCase() || key.toLowerCase().includes(input) || singularize(key.toLowerCase()) === input) {
             // Return the values
-            return [key, value];
+            return value;
             
         } else {
           // Return Destination per city input
           value.forEach(element => {
             // Check Input via country
             if (element.name.toLowerCase().includes(input)) {
-                console.log(element);
-                searchList += element;
+                searchList = element;
             // Check input via city name and make sure isn't undefined
             } else if (element.cities) {
                 if (element.cities.some((i) => i.name.toLowerCase().includes(input))) {
-                    searchList += element.cities.find((item) => item.name.toLowerCase().includes(input));
+                    searchList = element.cities.find((item) => item.name.toLowerCase().includes(input));
                 }
                 
             }
@@ -59,6 +74,14 @@ function lookUp(array, input) {
     return searchList;
 };
 
+function displayResult(destination) {
+    if (destination) {
+        let [ref, obj] = destination.split(",");
+        console.log(ref);
+        console.dir(obj);
+        console.log(obj);
+    }
+}
 // To singularize a word if it not found in the JSON Data.
 function singularize(word) {
     const endings = {
